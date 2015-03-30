674240d25e06e685fec255985b33f83ef539a554
Luke Hsiao <luke.w.hsiao@gmail.com>
2015-03-30 15:41:20 -0600

    1st implementation of verify_readback_word

--- src/Xilinx_Readback_Verify.c
+++ src/Xilinx_Readback_Verify.c
@@ -6,18 +6,21 @@ unsigned convert_ascii_to_binary(char* ascii_string) {
   unsigned result;
   result = 0;
   
-  printf("Given ascii: ");
   int i;
   for (i = 0; i < 32; i++) {
-    printf("%c", ascii_string[i]);
     //ascii_string - 0x30 will convert ascii 0 and 1 to actual 0 and 1
     result = result | ((ascii_string[i]-0x30) << (31-i));
   }
-  printf("\n");
   return result;
 }
 
 // Given the binary values of a word in a frame, determine if they are equal
 unsigned verify_readback_word(unsigned data, unsigned gold, unsigned mask) {
-  return 1;
+  
+  // Mask Gold with Mask
+  unsigned masked_gold = gold & (~mask);
+  
+  // compare and return
+  
+  return (data == masked_gold);
 }
