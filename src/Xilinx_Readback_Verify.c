9a5297a124e413e77063d48ca16df775d3f2fb3f
Luke Hsiao <luke.w.hsiao@gmail.com>
2015-03-30 11:41:24 -0600

    Added simple src test

--- src/Xilinx_Readback_Verify.c
+++ src/Xilinx_Readback_Verify.c
@@ -1,4 +1,6 @@
 #include "Xilinx_Readback_Verify.h"
+#include <assert.h>
+#include <stdio.h>
 
 
 int travis_test() {
@@ -6,5 +8,9 @@ int travis_test() {
 }
 
 int main(int argc, char *argv[]) {
+  int result;
+  result = travis_test();
+  assert(88 == travis_test());
+  printf("PASSED!\n\n");
   return 0;
 }
