901c2a280e73365f1a6d66e4654c9dd92b23f657
Luke Hsiao <luke.w.hsiao@gmail.com>
2015-03-30 15:11:08 -0600

    test should build and fail

--- src/Xilinx_Readback_Verify.c
+++ src/Xilinx_Readback_Verify.c
@@ -4,6 +4,13 @@
 // Given a 32 character string of 0s and 1s, returns the equivalent 32-bit int
 unsigned convert_ascii_to_binary(char* ascii_string) {
   unsigned result;
+  result = 0;
+  
+  int i;
+  for (i = 0; i < 32; i++) {
+    //ascii_string - 0x30 will convert ascii 0 and 1 to actual 0 and 1
+    result = result & (ascii_string[i]-0x30 << (31-i));
+  }
   
   return 0;
 }
--- src/Xilinx_Readback_Verify.h
+++ src/Xilinx_Readback_Verify.h
@@ -1,6 +1,5 @@
 
 #define WORD_SIZE 33 // extra bit for null terminator
 
-int convert_ascii_to_binary(char* ascii_string);
+unsigned convert_ascii_to_binary(char* ascii_string);
 
-int travis_test();
